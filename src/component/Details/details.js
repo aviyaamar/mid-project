f39e3f7ce5f00f5ae7fc83e5cc95b2069111805d
aviyaamar <aviyaamar1@gmail.com>
2022-01-05 23:41:07 +0200

    fixed details

--- src/component/Details/details.js
+++ src/component/Details/details.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useEffect, useState, useContext } from "react";
 import {useParams, useHistory } from "react-router-dom";
 import axios from 'axios';
